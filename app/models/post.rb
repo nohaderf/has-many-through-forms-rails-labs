d9c5600d4a736166a756b460f06cd3e28ff2cb5b
nohaderf <freda.hon@gmail.com>
2020-10-29 18:25:26 -0400

    commit tree 9158a6ebf53ec53c55e6ab908cb5dfa545f0a044

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,16 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
+  accepts_nested_attributes_for :categories
   has_many :comments
   has_many :users, through: :comments
 
-
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if category_attribute["name"].present?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
+    end
+  end
 end
